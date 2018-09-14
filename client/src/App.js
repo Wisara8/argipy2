5e4219a2da95720f246f13bbddfb3dbc0a199334
JoelCodes <amnshinness@yahoo.com>
2018-09-13 19:44:28 -0700

    Added connection to socket server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    this.socket = new WebSocket('ws://localhost:8080');
+    this.socket.onmessage = (event) => {
+      console.log('Message', event);
+    }
+  }
   render() {
     return (
       <div className="App">
